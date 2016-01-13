125cef8d9540b280b24247c23cd9c023a988875f
Victor Borja <vborja@apache.org>
2016-01-13 07:03:21 -0600

    v1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule OkJose.Mixfile do
 
   def project do
     [app: :ok_jose,
-     version: "0.0.1",
+     version: "1.0.0",
      description: description,
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
