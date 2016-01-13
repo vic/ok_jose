a34c435c998dc40e0bd30ea82388924e2a38dffd
Victor Borja <vborja@apache.org>
2016-01-13 02:50:12 -0600

    remove unused files from package

--- mix.exs
+++ mix.exs
@@ -26,11 +26,12 @@ defmodule OkJose.Mixfile do
   end
 
   defp package do
-    [files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+    [files: ["lib", "mix.exs", "README*"],
      maintainers: ["Victor Borja"],
      licenses: ["Apache 2.0"],
      links: %{"GitHub" => "https://github.com/vic/ok_jose"}]
   end
+
   # Dependencies can be Hex packages:
   #
   #   {:mydep, "~> 0.3.0"}
